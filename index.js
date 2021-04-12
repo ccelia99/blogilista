1d2e94019581bf81d1fa93e2932725ddc1ff7c41
ccelia99 <serja.aaltonen@gmail.com>
2021-04-12 20:20:05 +0300

    Teht. 4.1 valmiina

--- index.js
+++ index.js
@@ -9,15 +9,37 @@ app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+}
+
+const morgan = require('morgan')
+morgan.token('body', function (req) {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.get('/api/blogs', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
+    .catch(error => {
+        next(error)
+    })
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
   const blog = new Blog(request.body)
 
   blog
@@ -25,9 +47,22 @@ app.post('/api/blogs', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+    .catch(error => {
+        next(error)
+      })
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+  
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -19,7 +19,7 @@ const blogSchema = mongoose.Schema({
     likes: Number
 })
 
-personSchema.set('toJSON', {
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
