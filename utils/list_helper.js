e785072c8c2e3230a79b1b6abc8ba2d929c592c4
ccelia99 <serja.aaltonen@gmail.com>
2021-04-12 22:46:01 +0300

    Teht. 4.4 valmiina

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const likeArray = blogs.map(like =>
+    like.likes)
+  const reducer = (sum, item) => {
+    return sum + item
   }
+  return likeArray.length === 0
+    ? 0 
+    : likeArray.reduce(reducer, 0)
+}
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+  dummy,
+  totalLikes
+}
