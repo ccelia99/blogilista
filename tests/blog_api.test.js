a68d28d717a4de8fc6878b58aaeb309d2e2ad6e4
ccelia99 <serja.aaltonen@gmail.com>
2021-04-14 01:51:38 +0300

    Teht. 4.11 valmiina

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -53,6 +53,26 @@ test('a valid blog can be added ', async () => {
   )
 })
 
+test('a blog with undefined likes will be set as 0 likes', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()  
+  const likesValue = blogsAtEnd[blogsAtEnd.length-1].likes
+  console.log('likesValue', likesValue)
+  expect(likesValue).toBe(0)  
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
