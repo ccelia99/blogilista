88fc63a35fa948bde83d82a2cae775587ae8b71c
ccelia99 <serja.aaltonen@gmail.com>
2021-04-14 01:33:37 +0300

    Teht. 4.10 valmiina

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -30,6 +30,29 @@ test('a field specifying a blog is named as id', async () => {
   response.forEach(item => expect(item.id).toBeDefined())
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5    
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+  const titles = blogsAtEnd.map(n => n.title)
+  expect(titles).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
